8bab55a375f4db2617553214969887f461730325
GauravRaiBhatt <gaurav.rai.bhatt@gmail.com>
2024-10-30 19:00:37 +0530

    moved images to public folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     <header className="flex justify-between items-center py-1 px-4 bg-slate-400 text-white w-full border-solid border-2 border-b-black fixed">
       {/* Left side: Logo and Title */}
     {/* <img src="/src/assets/logo1.jpeg" alt="Logo" className="h-8 mr-2" /> */}
-    <img src="./assets/logo2.webp" alt="Logo" className="h-8 mr-2 myBorderClass" />
+    <img src="../../logo2.webp" alt="Logo" className="h-8 mr-2 myBorderClass" />
       {/* <div className="flex items-center">
         <img src="/src/assets/react.svg" alt="Logo" className="h-8 mr-2" />
         <h1 className="text-xl font-bold text-black sm:hidden">The GRB's</h1>
@@ -25,7 +25,7 @@ const Header = () => {
           <button className="text-white hover:text-green-500">
             {/* <FontAwesomeIcon icon={faWhatsapp} size="lg" /> */}
             <img
-              src="./assets/whatsapp-brands-solid.svg"
+              src="../../whatsapp-brands-solid.svg"
               alt="Logo"
               className="h-8 mr-2"
             />
